bfc2506d480d6b9c8f6249463c7591503fc8f96f
SebastianDevus <sebastiandevus@gmail.com>
2022-05-13 22:39:35 -0300

    Exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int numeroEscolhido, contador = 0;
+
+Console.Write("\nDigite um número: ");
+numeroEscolhido = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\nOs números pares entre 0 e {numeroEscolhido} são:");
+
+while (contador <= numeroEscolhido)
+{
+    Console.Write($"{contador} ");
+    contador = contador + 2;
+}
+
+Console.WriteLine("\n");
